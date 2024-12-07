119697871d9f868f80e7b1069e72e6f708db2944
Squiddyvert <samuelfrancosq@gmail.com>
2024-12-06 19:48:26 -0500

    added functionality to output BMI

--- interface.py
+++ interface.py
@@ -16,8 +16,8 @@ class GUI:
         quitButton = tkinter.Button(self.root, text="Quit", command=self.root.destroy)
         quitButton.pack()
 
-        # List to store profiles
-        self.profiles = [] 
+        # Dictionary to store profiles based off name
+        self.profiles = {}
 
         #creates tabs for the application
         self.home_Tab()
@@ -49,42 +49,52 @@ class GUI:
     def profile_Tab(self):
         self.profileTab = tkinter.Frame(self.my_notebook)
         self.my_notebook.add(self.profileTab, text= "log profile")
+        self.profileTab.grid_columnconfigure(1, weight=1)
 
         nameLabel = tkinter.Label(self.profileTab,text="name")
-        nameLabel.pack()
+        nameLabel.pack(side = "top", anchor= "nw")
         self.nameEntry = tkinter.Entry(self.profileTab,width=30)
-        self.nameEntry.pack()
+        self.nameEntry.pack(side = "top", anchor= "nw")
 
         self.gender = tkinter.IntVar()
         self.gender.set(0)
 
         mButton = tkinter.Radiobutton(self.profileTab,text="male",variable=self.gender, value=0)
         fButton = tkinter.Radiobutton(self.profileTab,text="female",variable=self.gender, value=1)
-        mButton.pack()
-        fButton.pack()
+        mButton.pack(side = "top", anchor= "nw")
+        fButton.pack(side = "top", anchor= "nw")
 
         ageLabel = tkinter.Label(self.profileTab,text="age")
-        ageLabel.pack()
+        ageLabel.pack(side = "top", anchor= "nw")
         self.ageEntry = tkinter.Entry(self.profileTab,width=10)
-        self.ageEntry.pack()
+        self.ageEntry.pack(side = "top", anchor= "nw")
 
-        heightLabel = tkinter.Label(self.profileTab,text="height")
-        heightLabel.pack()
+        heightLabel = tkinter.Label(self.profileTab,text="Height(in)")
+        heightLabel.pack(side = "top", anchor= "nw")
         self.heightEntry = tkinter.Entry(self.profileTab,width=10)
-        self.heightEntry.pack()
+        self.heightEntry.pack(side = "top", anchor= "nw")
 
-        weightLabel = tkinter.Label(self.profileTab,text="weight")
-        weightLabel.pack()
+        weightLabel = tkinter.Label(self.profileTab,text="Weight(lbs)")
+        weightLabel.pack(side = "top", anchor= "nw")
         self.weightEntry = tkinter.Entry(self.profileTab,width=10)
-        self.weightEntry.pack()
+        self.weightEntry.pack(side = "top", anchor= "nw")
 
         profileSaveButton = tkinter.Button(self.profileTab, text="Save Profile", command= lambda: func.saveProfile(self))
-        profileSaveButton.pack()
+        profileSaveButton.pack(side = "top", anchor= "nw")
 
         #creates button that outputs a profile
         outputButton = tkinter.Button(self.profileTab, text="Output Profile", command= lambda: func.outputProfile(self))
-        outputButton.pack(pady=5)
+        outputButton.pack(side = "top", anchor= "nw",pady=5)
         
+        #creates a section where user can choose current profile and output their bmi and bmi category
+        profileLabel = tkinter.Label(self.profileTab,text="Select the profile you wish to continue with")
+        profileLabel.pack(side ="top", anchor= "center")
+        self.currentProfileEntry = tkinter.Entry(self.profileTab,width=10)
+        self.currentProfileEntry.pack(side ="top", anchor= "center")
+        bmiOutputButton = tkinter.Button(self.profileTab, text="Click here to see your BMI and BMI category based off your profile", command= lambda: func.bmiLevel(self,func.getBMI(self,self.currentProfileEntry.get())) )
+        bmiOutputButton.pack(side = "top", anchor = "center", pady=5)
+        
+        #creates button that takes user back to home
         homeButton = tkinter.Button(self.profileTab, text="Home", command=lambda: self.my_notebook.select(0))
         homeButton.pack(side=tkinter.BOTTOM, pady=5)
 
@@ -101,7 +111,7 @@ class GUI:
 
         self.dropdown_options = func.workoutNames()
 
-        save_button = tkinter.Button(self.createLogTab, text="Save", command=self.save_data)
+        save_button = tkinter.Button(self.createLogTab, text="Save", command= lambda: func.save_data(self))
         save_button.pack(side='bottom', padx=5)
 
         self.row_counter = 0
