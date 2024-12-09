a8b54dc44eec0f53e2feb42efbb0e9f19371d0ab
Squiddyvert <samuelfrancosq@gmail.com>
2024-12-09 15:35:36 -0500

    Merge branches 'main' and 'master' of https://github.com/SquiddyVert/fitness-project

--- set.py
+++
@@ -1,7 +0,0 @@
-class Set:
-    def __init__(self, reps, weight):
-        self.reps = reps
-        self.weight = weight
-
-    def get_summary(self):
-        return f"{self.reps} reps, {self.weight} lbs"
